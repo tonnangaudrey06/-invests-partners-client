c73f368ccd213c6b3039efa04ac47bd82f5033d8
BeatJo <eelongivan85@gmail.com>
2021-09-21 10:09:05 +0200

    main

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './styles/App.css';
+import AppNavigator from './navigation'
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavigator />
+    </div>
+  );
+}
+
+export default App;
