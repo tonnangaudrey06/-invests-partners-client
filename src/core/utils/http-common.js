ac29791f8617cfff787e741440accb242cb1903d
Eddy Harold <donfackeddy12@gmail.com>
2021-10-08 14:52:22 +0100

    Update

--- src/core/utils/http-common.js
+++ src/core/utils/http-common.js
@@ -3,7 +3,7 @@ import config from './config';
 import localStorage from './localstorage';
 
 const instance = axios.create({
-    baseURL: config.URL,
+    baseURL: config.HOSTURL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
         'Accept': 'application/json'
